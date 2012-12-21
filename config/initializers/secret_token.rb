e4c6e3681e04f00b512fbc13b51884af13fbcc0f
Ilya Zayats <somebody32@gmail.com>
2012-12-21 11:52:00 +0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Papermaps::Application.config.secret_token = '878c00cf612296640cae3f155396c0cdba31214f28b91041930042a01b959bb1d0b4f7b8ddbdb415320c1b6b23b8de6c883fd730432e074345be1ec1e03701ac'
