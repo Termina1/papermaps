532bc1b87fd8c181c69f76c3d3e1edddb6fa74d2
Ilya Zayats <somebody32@gmail.com>
2012-12-21 14:15:09 +0400

    added something

--- app/models/concerns/riak_storage.rb
+++ app/models/concerns/riak_storage.rb
@@ -4,14 +4,36 @@ module RiakStorage
   BUCKET_NAME = "photos"
 
   included do
-    attr_accessor :client, :bucket
+    attr_accessor :object
     cattr_accessor :client, :bucket
 
     self.client ||= Riak::Client.new
     self.bucket ||= client.bucket BUCKET_NAME
   end
 
-  def store
-    object.store
+  module ClassMethods
+    private
+
+    def get_ordered_by_date(count)
+      map = <<-END
+      function(v) {
+        return [JSON.parse(v.values[0].data)];
+      };
+      END
+
+      reduce = <<-END
+        function (v, args) {
+          v.sort( function(a,b) { return b['lastModifiedParsed'] - a['lastModifiedParsed'] });
+          return v;
+        };
+      END
+
+      Riak::MapReduce.new(client).
+                  add(BUCKET_NAME).
+                  map(map).run
+    end
+  end
+
+  module InstanceMethods
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,9 @@
 class Photo
   include RiakStorage
+
+  attr_accessor :username, :url, :gps, :description, :date
+
+  def self.get_last(count=50)
+    get_ordered_by_date count
+  end
 end
