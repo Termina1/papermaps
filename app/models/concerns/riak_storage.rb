2aba7867114972c0fedcdcb7bf162ae1a15794a7
Ilya Zayats <somebody32@gmail.com>
2012-12-21 17:04:49 +0400

    finished riak loading

--- app/models/concerns/riak_storage.rb
+++ app/models/concerns/riak_storage.rb
@@ -12,6 +12,16 @@ module RiakStorage
   end
 
   module ClassMethods
+    def get_last_saved_photo_id
+      get('last_photo_id').data
+    end
+
+    def save_last_saved_photo_id(id)
+      marker = get('last_photo_id')
+      marker.data = { id: id, type: "system" }
+      marker.store
+    end
+
     def get(key)
       bucket.get_or_new key
     end
@@ -19,16 +29,31 @@ module RiakStorage
     private
 
     def get_ordered_by_date(count)
-      map = " function(v) { return [v]; } "
+      map = 'function(v) { var ret = []; var o = Riak.mapValuesJson(v)[0]; if (o.type == "photo") { ret.push(v); } return ret; }'
 
-      reduce = 'function (v) { v.sort( function(a,b) { return b["metadata"]["X-Riak-Last-Modified"] - a["metadata"]["X-Riak-Last-Modified"] }); return v; }'
+      reduce = 'function (v) { v.sort( function(a,b) { return b["values"][0]["metadata"]["X-Riak-Last-Modified"] - a["values"][0]["metadata"]["X-Riak-Last-Modified"] }); return v; }'
 
-      Riak::MapReduce.new(client).
+      finder_result = Riak::MapReduce.new(client).
                   add(BUCKET_NAME).
                   map(map, keep: true).reduce(reduce).run
+
+      objects = Riak::RObject.load_from_mapreduce client, finder_result
+      photos = []
+
+      objects.each do |o|
+        data = o.data
+        data.delete "type"
+
+        photos << new(data)
+      end
+
+      photos
     end
   end
 
-  module InstanceMethods
+  def save
+    object = self.class.get id
+    object.data = instance_values.merge(type: "photo")
+    object.store
   end
 end
